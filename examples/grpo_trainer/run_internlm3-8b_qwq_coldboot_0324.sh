63ce6d6a7fc0ef59a3db1df351218ac09a9c2a07
dbinthesky <johntongjian@163.com>
2025-03-30 09:11:34 +0800

    update

--- examples/grpo_trainer/run_internlm3-8b_qwq_coldboot_0324.sh
+++ examples/grpo_trainer/run_internlm3-8b_qwq_coldboot_0324.sh
@@ -95,7 +95,7 @@ run_training() {
         algorithm.adv_estimator="grpo" \
         data.train_files="${TRAIN_DATA}" \
         data.val_files="${VAL_DATA}" \
-        data.train_batch_size=64 \
+        data.train_batch_size=128 \
         data.max_prompt_length=1024 \
         data.max_response_length=31744 \
         data.filter_overlong_prompts=True \
@@ -122,7 +122,7 @@ run_training() {
         actor_rollout_ref.rollout.max_num_batched_tokens=300000 \
         actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
         actor_rollout_ref.rollout.temperature=1.0 \
-        actor_rollout_ref.rollout.n=8 \
+        actor_rollout_ref.rollout.n=4 \
         +actor_rollout_ref.rollout.trust_remote_code=True \
         actor_rollout_ref.rollout.log_prob_micro_batch_size=8 \
         +actor_rollout_ref.rollout.n_val=1 \
