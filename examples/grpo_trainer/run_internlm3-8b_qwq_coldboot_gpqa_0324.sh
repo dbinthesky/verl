f258577c781083c3f0724449ab4ec9804f9cc3fb
dbinthesky <johntongjian@163.com>
2025-03-31 20:20:43 +0800

    update

--- examples/grpo_trainer/run_internlm3-8b_qwq_coldboot_gpqa_0324.sh
+++ examples/grpo_trainer/run_internlm3-8b_qwq_coldboot_gpqa_0324.sh
@@ -111,7 +111,7 @@ run_training() {
         actor_rollout_ref.actor.ppo_max_token_len_per_gpu=32768 \
         actor_rollout_ref.actor.use_kl_loss=True \
         actor_rollout_ref.actor.kl_loss_coef=0.0 \
-        actor_rollout_ref.actor.entropy_coeff=0.00 \
+        actor_rollout_ref.actor.entropy_coeff=0.001 \
         actor_rollout_ref.actor.kl_loss_type="low_var_kl" \
         actor_rollout_ref.model.enable_gradient_checkpointing=True \
         +actor_rollout_ref.model.trust_remote_code=True \
