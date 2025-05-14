1f0e078a3cc5e3d35a9d413b10fb7ae251e02f02
dbinthesky <johntongjian@163.com>
2025-05-14 12:21:37 +0800

    update

---
+++ rewards/fabricate_qa.py
@@ -0,0 +1,217 @@
+import re
+import jieba
+import random
+import requests
+from functools import partial
+from abc import abstractmethod
+from typing import Any, Dict, Callable
+from tqdm import tqdm as tqdm_nonasync
+from collections import namedtuple, defaultdict
+
+
+from openai import OpenAI, RateLimitError, AsyncOpenAI, RateLimitError
+from tenacity import retry, stop_after_attempt, wait_exponential
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+# BASE
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+RM_URLS = [
+    "http://10.130.0.174:5020"
+]
+VERIFIER_MODEL_NAME = "qwen25_7B_instruct"
+VERIFIER_MODEL_PATH = "http://10.130.247.138:8000/v1"
+DEFAULT_PARSE_FAILURE_REWARD = -2.
+
+
+class APIError(Exception):
+    pass
+
+
+class PostprocessError(Exception):
+    pass
+
+
+class Agent:
+    def __init__(
+        self,
+        system: str | None = None,
+        model: str = "gpt-3.5-turbo",
+        base_url: str | None = None,
+        api_keys: str | list[str] | None = None,
+        request_kwargs: dict[str, Any] = None,
+    ):
+        self.system = system
+        if self.system is None:
+            self.history = []
+        else:
+            self.history = [{"role": "system", "content": self.system}]
+        self.model = model
+        self.base_url = base_url
+
+        if api_keys is not None:
+            pass
+        else:
+            api_keys = [os.getenv("OPENAI_API_KEY", "EMPTY")]
+        self.api_keys = api_keys
+
+        self.request_kwargs = {
+            "max_tokens": 1024,
+            "temperature": 0.6,
+            "top_p": 0.95,
+            "seed": 100745534,
+        }
+        if request_kwargs is not None:
+            self.request_kwargs.update(request_kwargs)
+
+    async def run(self, messages, max_concurrent, desc, postprocess_fns):
+        semaphore = asyncio.Semaphore(max_concurrent)
+        async with AsyncOpenAI(api_key=self.api_keys, base_url=self.base_url) as client:
+            results = []
+            tasks = [self.process_prompt(client, message, semaphore, postprocess_fn)
+                     for message, postprocess_fn in zip(messages, postprocess_fns)]
+
+            if desc is not None:
+                for f in tqdm.asyncio.tqdm.as_completed(tasks, dynamic_ncols=True, desc=desc):
+                    results.append(await f)
+            else:
+                try:
+                    results = await asyncio.gather(*tasks)
+                except Exception as err:
+                    print(f'[ERROR] asyncio.gather failed: {err}')
+                    return None
+            return results
+
+    @retry(stop=stop_after_attempt(4), wait=wait_exponential(multiplier=1, min=5, max=20))
+    async def chat_completion(self, client, messages, postprocess_fn) -> str | None:
+        response = None
+        try:
+            response = await client.chat.completions.create(
+                model=self.model, messages=[{
+                    "role": "user", "content": messages
+                }], **self.request_kwargs,
+            )
+            return postprocess_fn(response.choices[0].message.content)
+        except PostprocessError as e:
+            print(
+                f"[ERROR] failure occurred when parse result: (response={response}), {e}")
+            raise PostprocessError("Failed to generate text")
+        except Exception as e:
+            print(
+                f"[ERROR] failure occurred when call API: {e} (response={response})")
+            raise APIError("Failed to generate text")
+
+    async def process_prompt(self, client, messages, semaphore, postprocess_fn):
+        async with semaphore:
+            try:
+                result = await self.chat_completion(client, messages, postprocess_fn)
+                return messages, result
+            except Exception as err:
+                return messages, None
+
+
+agent = Agent(**{
+    "model": VERIFIER_MODEL_NAME,
+    "base_url": VERIFIER_MODEL_PATH,
+    "api_keys": "EMPTY",
+    "request_kwargs": {
+        "temperature": 0.75,
+        "timeout": 180,
+        "max_tokens": 2048
+    },
+})
+
+
+class PenaltyOrReward(object):
+    @abstractmethod
+    def get_penalty_or_reward(self, solution_str, ground_truth, lang_code=None):
+        raise NotImplementedError
+
+
+def batchify(iterable, n):
+    batch = []
+    for item in iterable:
+        batch.append(item)
+        if len(batch) == n:
+            yield batch
+            batch = []
+    if batch:
+        yield batch
+
+
+def contain_chinese(string):
+    try:
+        pattern = re.compile(r'[\u4e00-\u9fa5]')
+        if re.search(pattern, string):
+            return True
+        return False
+    except Exception as err:
+        return False
+
+
+def postprocess_solution(solution_str):
+    if "<|im_end|>" in solution_str:
+        return solution_str[:solution_str.index("<|im_end|>")]
+    return solution_str
+
+
+def rm_request_with_retry(RM_URLS, data, max_retries=3, retry_delay=1, suffix="/reward"):
+    retries = 0
+    while retries < max_retries:
+        try:
+            url = random.choice(RM_URLS)
+            response = requests.post(f'{url}{suffix}', json=data, timeout=600)
+            response.raise_for_status()  # 如果状态码不是 200，抛出异常
+            return response.json()
+        except requests.RequestException as e:
+            print(f"请求(数据总量={len(data)})失败，错误信息: {e}，重试第 {retries + 1} 次...")
+            retries += 1
+            if retries < max_retries:
+                time.sleep(retry_delay)
+    print("达到最大重试次数，请求失败。")
+    return None
+
+
+def compute_rm_score(
+        batch_solution_str,
+        batch_ground_truth,
+        postprocess_solution_fn,
+        parse_result_failure_score=DEFAULT_PARSE_FAILURE_REWARD,
+        judge_prompt_key="ground_truth"
+):
+    input_datas = []
+    rewards = {}
+
+    for i, (solution_str, ground_truth) in enumerate(zip(batch_solution_str, batch_ground_truth)):
+        solution_str = postprocess_solution_fn(solution_str)
+        if solution_str is None:
+            rewards[i] = parse_result_failure_score
+            continue
+        if ground_truth is None:
+            rewards[i] = parse_result_failure_score
+            continue
+
+        input_data = {
+            "prompt": ground_truth[judge_prompt_key], "response": solution_str, "id": i
+        }
+        input_datas.append(input_data)
+
+    if len(input_datas) > 0:
+        for batch in tqdm_nonasync(batchify(input_datas, n=128), desc=f'[RM][{RM_URLS}] batchify inference (batch=128)'):
+            output_datas = rm_request_with_retry(RM_URLS, batch)
+            for _ in output_datas['reward']:
+                _id = int(_["id"])
+                rewards[_id] = _["rm_score"]
+
+    final_results = []
+    for i in range(len(batch_solution_str)):
+        if i in rewards:
+            final_results.append(rewards[i])
+        else:
+            final_results.append(0.)
+    return final_results
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+# BASE
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
