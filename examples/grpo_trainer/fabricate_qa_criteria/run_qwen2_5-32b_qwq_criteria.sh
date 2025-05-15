619d85eb0f46ffeef9a47c00f837b48ee804591b
dbinthesky <johntongjian@163.com>
2025-05-16 01:18:42 +0800

    update

--- examples/grpo_trainer/fabricate_qa_criteria/run_qwen2_5-32b_qwq_criteria.sh
+++ examples/grpo_trainer/fabricate_qa_criteria/run_qwen2_5-32b_qwq_criteria.sh
@@ -49,16 +49,14 @@ setup_path() {
 
     CUSTOM_CODE_DIR="/cpfs01/shared/llm_ddd/tongjian/verl"
     VERL_DIR="/cpfs01/shared/llm_ddd/tongjian/verl"
-    # BASE_MODEL_PATH="/cpfs01/shared/llm_ddd/opencompass/models/hf_hub/models--deepseek-ai--DeepSeek-R1-Distill-Qwen-14B/snapshots/c79f47acaf303faabb7133b4b7b76f24231f2c8d"
-    # BASE_MODEL_PATH="/cpfs01/shared/llm_ddd/tongjian/ckpts/datareview_rl_test/verl/grpo/archived/qwen2_5-7b_qwq_fabricate_qa_criteria-dlc-2025-05-14-16-53-29_grpo_step_10"
-    BASE_MODEL_PATH="/cpfs01/shared/llm_ddd/tongjian/ckpts/datareview_rl_test/verl/grpo/archived/qwen2_5-7b_qwq_fabricate_qa_criteria-dlc-2025-05-15-07-39-43_grpo_step_10"
+    BASE_MODEL_PATH="/cpfs01/shared/llm_ddd/opencompass/models/hf_hub/models--deepseek-ai--DeepSeek-R1-Distill-Qwen-32B_new/snapshots/2d78713b01ecefe27a89fafec248a5dfd731396f"
     TRAIN_DATA="/cpfs01/shared/llm_ddd/tongjian/rl/fabricate_qa_criteria/super_gpqa_aio_noneasy_train_0513_criteria_train_0514.parquet"
     VAL_DATA="/cpfs01/shared/llm_ddd/tongjian/rl/fabricate_qa_criteria/super_gpqa_aio_noneasy_train_0513_criteria_test_0514.parquet"
 
-    experiment_name="qwen2_5-7b_qwq_fabricate_qa_criteria-dlc-${YYMMDD}-${HHMMSS}"
+    experiment_name="qwen2_5-32b_qwq_fabricate_qa_criteria-dlc-${YYMMDD}-${HHMMSS}"
     project_name="verl_grpo_qwq_fabricate_qa_criteria"
 
-    OUTPUT_DIR="/cpfs01/shared/llm_ddd/tongjian/ckpts/datareview_rl_test/verl/grpo/qwen2_5-7b_qwq_fabricate_qa_criteria/${experiment_name}/${YYMMDD}/${HHMMSS}"
+    OUTPUT_DIR="/cpfs01/shared/llm_ddd/tongjian/ckpts/datareview_rl_test/verl/grpo/qwen2_5-32b_qwq_fabricate_qa_criteria/${experiment_name}/${YYMMDD}/${HHMMSS}"
     mkdir -p "${OUTPUT_DIR}"
 }
 setup_path
@@ -99,18 +97,18 @@ run_training() {
         data.val_files="${VAL_DATA}" \
         data.train_batch_size=16 \
         data.max_prompt_length=2048 \
-        data.max_response_length=6144 \
+        data.max_response_length=4096 \
         data.filter_overlong_prompts=True \
         trainer.default_local_dir="${OUTPUT_DIR}" \
         actor_rollout_ref.model.path="${BASE_MODEL_PATH}" \
         actor_rollout_ref.actor.optim.lr=5e-7 \
         actor_rollout_ref.model.use_remove_padding=True \
         actor_rollout_ref.actor.shuffle=True \
-        actor_rollout_ref.actor.ppo_mini_batch_size=16 \
-        actor_rollout_ref.actor.ppo_micro_batch_size=$((total_gpus)) \
-        actor_rollout_ref.actor.ulysses_sequence_parallel_size=1 \
+        actor_rollout_ref.actor.ppo_mini_batch_size=8 \
+        actor_rollout_ref.actor.ppo_micro_batch_size=16 \
+        actor_rollout_ref.actor.ulysses_sequence_parallel_size=4 \
         actor_rollout_ref.actor.use_dynamic_bsz=True \
-        actor_rollout_ref.actor.ppo_max_token_len_per_gpu=8192 \
+        actor_rollout_ref.actor.ppo_max_token_len_per_gpu=6144 \
         actor_rollout_ref.actor.use_kl_loss=False \
         actor_rollout_ref.actor.kl_loss_coef=0.0 \
         actor_rollout_ref.actor.entropy_coeff=0.001 \
@@ -122,7 +120,7 @@ run_training() {
         actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
         actor_rollout_ref.rollout.name="vllm" \
         actor_rollout_ref.rollout.max_num_batched_tokens=300000 \
-        actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
+        actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
         actor_rollout_ref.rollout.temperature=1. \
         actor_rollout_ref.rollout.n=8 \
         +actor_rollout_ref.rollout.trust_remote_code=True \
