db5850c9226178f4c3bc54cba28453f8ed09624a
dbinthesky <johntongjian@163.com>
2025-06-10 14:23:09 +0800

    update

--- rewards/test_fabricate_qa.py
+++ rewards/test_fabricate_qa.py
@@ -416,6 +416,7 @@ class TestDoc2QueryV2(unittest.TestCase):
         print(verifier.verify("0.010 °"))
         print(verifier.verify("10.938 kg/m²"))
         print(verifier.verify("12288 KB"))
+        print(verifier.verify("1.256 × 10^-67 J"))
 
     def test_doc2query_v2_parse_solution_fn(self):
         print(doc2query_v2_parse_solution_fn(
@@ -426,24 +427,24 @@ class TestDoc2QueryV2(unittest.TestCase):
         ))
 
     def test_question_similarity(self):
-        batch_solution_str, batch_ground_truth = load_doc2query_v2()
+        batch_solution_str, batch_ground_truth=load_doc2query_v2()
 
-        x, y = [], []
-        task = QwQLongCoTDoc2QueryV2ComputeScore(split="valid")
+        x, y=[], []
+        task=QwQLongCoTDoc2QueryV2ComputeScore(split="valid")
         for solution_str, gt in zip(batch_solution_str, batch_ground_truth):
             # QSim\Format\AnsFeature
 
-            solution_str = '<think>***</think><question>\nQuestion: 在范畴Set中，设函子G为Option，对于任何集合A，Option(A)是{None} ∪ A。给定符号函子H由H(X) = X·G定义。设函子Z和自然变换e满足：对于任意集合A，Z(A) = A × {0,1}，且e_{Option(A)}将Option(A)中的每个元素映射为：None → (None, 0)，而a ∈ A → (Some a, 1)。考虑集合A = {1, 2}，计算配分律δ_{(Z,e)}在Option(Z(A)) → Z(Option(A))的映射中，元素Some((1,0))的像在Z(Option(A))中的第二个分量的值。注意：Option(Z(A))中的元素Some((1,0))表示Some包裹着Z(A)中的元素(1,0)。\nAnswer: \\boxed{-1}\nAnswer Type: NumericalAnswer\n</question>'
-            score = task.get_penalties()["Format"](solution_str, gt)
+            solution_str='<think>***</think><question>\nQuestion: 在范畴Set中，设函子G为Option，对于任何集合A，Option(A)是{None} ∪ A。给定符号函子H由H(X) = X·G定义。设函子Z和自然变换e满足：对于任意集合A，Z(A) = A × {0,1}，且e_{Option(A)}将Option(A)中的每个元素映射为：None → (None, 0)，而a ∈ A → (Some a, 1)。考虑集合A = {1, 2}，计算配分律δ_{(Z,e)}在Option(Z(A)) → Z(Option(A))的映射中，元素Some((1,0))的像在Z(Option(A))中的第二个分量的值。注意：Option(Z(A))中的元素Some((1,0))表示Some包裹着Z(A)中的元素(1,0)。\nAnswer: \\boxed{-1}\nAnswer Type: NumericalAnswer\n</question>'
+            score=task.get_penalties()["Format"](solution_str, gt)
             print(solution_str)
             print(score)
             print("="*80)
             break
 
     def test_compute_score(self):
-        batch_solution_str, batch_ground_truth = load_doc2query_v2(32)
+        batch_solution_str, batch_ground_truth=load_doc2query_v2(32)
 
-        task = QwQLongCoTDoc2QueryV2ComputeScore(split="valid")
+        task=QwQLongCoTDoc2QueryV2ComputeScore(split="valid")
         print(task.compute_score([None]*len(batch_solution_str),
                                  batch_solution_str, batch_ground_truth))
 
@@ -451,9 +452,9 @@ class TestDoc2QueryV2(unittest.TestCase):
 class TestDoc2Query(unittest.TestCase):
     def test_get_difficulty_reward(self):
         async def main():
-            batch_solution_str, batch_ground_truth = load_doc2query()
-            task = QwQLongCoTDoc2QueryComputeScore(split="valid")
-            results = await task.get_difficulty_reward(
+            batch_solution_str, batch_ground_truth=load_doc2query()
+            task=QwQLongCoTDoc2QueryComputeScore(split="valid")
+            results=await task.get_difficulty_reward(
                 [None] *
                 len(batch_solution_str), batch_solution_str, batch_ground_truth
             )
@@ -462,26 +463,26 @@ class TestDoc2Query(unittest.TestCase):
 
     def test_chat_completion_with_retry(self):
         async def main():
-            batch_solution_str, batch_ground_truth = load_doc2query(32*8)
-            task = QwQLongCoTDoc2QueryComputeScore(split="valid")
-            prompts = []
+            batch_solution_str, batch_ground_truth=load_doc2query(32*8)
+            task=QwQLongCoTDoc2QueryComputeScore(split="valid")
+            prompts=[]
 
-            instruct = 'Answer the following multiple choice question. There is only one correct answer. The last line of your response should be in the format "Answer: $LETTER" (without quotes), where LETTER is one of the option letters. You must first think step by step with very detail thinking process.'
+            instruct='Answer the following multiple choice question. There is only one correct answer. The last line of your response should be in the format "Answer: $LETTER" (without quotes), where LETTER is one of the option letters. You must first think step by step with very detail thinking process.'
 
             for _ in batch_solution_str:
-                result = doc2query_parse_solution_fn(_)
+                result=doc2query_parse_solution_fn(_)
                 if result is None:
                     continue
-                question, options, answer = result
+                question, options, answer=result
                 prompts.append(task.format_question(
                     question, options, None) + f"\n\n{instruct}")
 
             # results = await task.chat_completion_with_retry(
             #     "http://10.130.0.245:5002", prompts
             # )
-            prompts = prompts * 16
-            s1 = time.time()
-            results = await task.generate_responses(
+            prompts=prompts * 16
+            s1=time.time()
+            results=await task.generate_responses(
                 prompts
             )
             # for p, r in zip(prompts, results):
@@ -492,42 +493,42 @@ class TestDoc2Query(unittest.TestCase):
         aio.run(main())
 
     def test_compute_score(self):
-        batch_solution_str, batch_ground_truth = load_doc2query(32)
+        batch_solution_str, batch_ground_truth=load_doc2query(32)
 
-        task = QwQLongCoTDoc2QueryComputeScore(split="valid")
+        task=QwQLongCoTDoc2QueryComputeScore(split="valid")
         print(qwq_longcot_doc2query_compute_score_valid([None]*len(batch_solution_str),
                                                         batch_solution_str, batch_ground_truth))
 
 
 def doc2query_format_filter(path):
-    outputs = []
+    outputs=[]
     with open(path, "rt") as f:
         for line in f:
-            example = json.loads(line)
-            prompt = example["self_improvement"]["chat_completion"]
-            option_num = int(re.findall(
+            example=json.loads(line)
+            prompt=example["self_improvement"]["chat_completion"]
+            option_num=int(re.findall(
                 r'- Number of options: (\d+)', prompt)[0].strip())
-            difficulty = re.findall(
+            difficulty=re.findall(
                 r'- Difficulty level: (\w+)', prompt)[0].strip()
 
-            response = example["self_improvement"]["responses"][0]["response"]["text"]
-            result = doc2query_parse_solution_fn(response)
+            response=example["self_improvement"]["responses"][0]["response"]["text"]
+            result=doc2query_parse_solution_fn(response)
             if result is None:
                 continue
-            question, options, answer = result
+            question, options, answer=result
             if len(options) != option_num:
                 continue
             try:
-                ans_index = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
+                ans_index=['A', 'B', 'C', 'D', 'E', 'F', 'G',
                              'H', 'I', 'J', 'K', 'L', 'M'].index(answer)
             except Exception as err:
                 continue
             if not (ans_index <= (len(options)-1)):
                 continue
-            prompt = prompt[prompt.index("<|im_start|>user") +
+            prompt=prompt[prompt.index("<|im_start|>user") +
                             len("<|im_start|>user"):prompt.index("<|im_end|>\n<|im_start|>assistant")].strip()
 
-            output = {
+            output={
                 "system": "You are a helpful assistant and an expert in creating a question. Now your task is to create a question. You first thinks about the reasoning process in the mind and then provides the user with the answer. Show your reasoning process in <think> </think> tags, and your final question creation in <question> </question> tags. Remember the part in <question> </question> tags MUST only be the question you created ** WITHOUT ** any explanation or solution.",
                 "instruction": prompt,
                 "input": "",
@@ -536,7 +537,7 @@ def doc2query_format_filter(path):
             outputs.append(output)
 
     with open("/cpfs01/shared/llm_ddd/tongjian/synthetic/sft/fabricate_qa_v2.json", "rt") as f:
-        mix = json.load(f)
+        mix=json.load(f)
         print(len(mix))
 
     mix.extend(outputs)
@@ -548,31 +549,31 @@ def doc2query_format_filter(path):
 
 
 def doc2query_bon_merge(path, output):
-    outputs = []
-    scorer = RuleBasedOptionMatch()
-    group = defaultdict(list)
+    outputs=[]
+    scorer=RuleBasedOptionMatch()
+    group=defaultdict(list)
 
-    pbar = tqdm(total=736928)
+    pbar=tqdm(total=736928)
 
     with open(path, "rt") as f:
         for line in f:
-            example = json.loads(line)
+            example=json.loads(line)
             pbar.update(1)
-            prompt = example["self_improvement"]["chat_completion"]
-            response = example["self_improvement"]["responses"][0]["response"]["text"]
-            result = doc2query_parse_solution_fn(response)
+            prompt=example["self_improvement"]["chat_completion"]
+            response=example["self_improvement"]["responses"][0]["response"]["text"]
+            result=doc2query_parse_solution_fn(response)
             if result is None:
                 continue
-            question, options, answer = result
+            question, options, answer=result
             try:
-                ans_index = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
+                ans_index=['A', 'B', 'C', 'D', 'E', 'F', 'G',
                              'H', 'I', 'J', 'K', 'L', 'M'].index(answer)
             except Exception as err:
                 continue
             if not (ans_index <= (len(options)-1)):
                 continue
 
-            score = scorer.get_penalty_or_reward(response, {
+            score=scorer.get_penalty_or_reward(response, {
                 "question": example["self_improvement"]["question"],
                 "answer": example["self_improvement"]["answer"],
                 "options": example["self_improvement"]["options"],
@@ -583,30 +584,30 @@ def doc2query_bon_merge(path, output):
                 "score": score
             })
 
-    done = {}
+    done={}
     with open(output, 'wt') as g:
         with open(path, "rt") as f:
             for line in f:
-                example = json.loads(line)
+                example=json.loads(line)
                 if example["uuid"] not in done:
-                    done[example["uuid"]] = True
+                    done[example["uuid"]]=True
                     del example["self_improvement"]["responses"]
-                    example["self_improvement"]["responses"] = group[example["uuid"]]
+                    example["self_improvement"]["responses"]=group[example["uuid"]]
                     g.write(f'{json.dumps(example, ensure_ascii=False)}\n')
 
 
 def doc2query_difficulty_filter(path, output):
-    pbar = tqdm(total=736928)
+    pbar=tqdm(total=736928)
 
     with open(output, "wt") as g:
         with open(path, "rt") as f:
             for line in f:
-                example = json.loads(line)
+                example=json.loads(line)
                 pbar.update(1)
-                responses = example["self_improvement"]["responses"]
+                responses=example["self_improvement"]["responses"]
 
-                threshold = 0.5
-                filtered = [_ for _ in responses if _["score"] > threshold]
+                threshold=0.5
+                filtered=[_ for _ in responses if _["score"] > threshold]
                 if len(filtered) < 7 and len(filtered):
                     del example["self_improvement"]["responses"]
                     del example["self_improvement"]["chat_completion"]
@@ -616,12 +617,12 @@ def doc2query_difficulty_filter(path, output):
 def doc2query_fabricate_qa_difficulty_reward(path, output):
 
     for filename in tqdm(os.listdir(path)):
-        basename = os.path.basename(filename)
-        outputs = []
-        filename = os.path.join(path, filename)
+        basename=os.path.basename(filename)
+        outputs=[]
+        filename=os.path.join(path, filename)
         with open(filename, "rt") as f:
             for line in f:
-                example = json.loads(line)
+                example=json.loads(line)
                 outputs.append(
                     (
                         example, example["self_improvement"]["responses"][0]["response"]["text"],
@@ -630,69 +631,69 @@ def doc2query_fabricate_qa_difficulty_reward(path, output):
                         }
                     )
                 )
-        output_path = f'{output}_{basename}'
+        output_path=f'{output}_{basename}'
         if os.path.exists(output_path):
             continue
 
         async def main():
-            task = QwQLongCoTDoc2QueryComputeScore(
+            task=QwQLongCoTDoc2QueryComputeScore(
                 split="valid", difficulty_bon=4)
             with open(output_path, "wt") as f:
                 for batch in batchify(outputs, n=1024):
-                    examples = [_[0] for _ in batch]
-                    batch_solution_str, batch_ground_truth = [
+                    examples=[_[0] for _ in batch]
+                    batch_solution_str, batch_ground_truth=[
                         _[1] for _ in batch], [_[2] for _ in batch]
-                    results, pass_rates = await task.get_difficulty_reward(
+                    results, pass_rates=await task.get_difficulty_reward(
                         [None] *
                         len(batch_solution_str), batch_solution_str, batch_ground_truth, repeat=4
                     )
                     for example, result, pass_rate in zip(examples, results, pass_rates):
-                        example["self_improvement"]["difficulty_reward"] = result
-                        example["self_improvement"]["pass_rate"] = pass_rate
+                        example["self_improvement"]["difficulty_reward"]=result
+                        example["self_improvement"]["pass_rate"]=pass_rate
                         f.write(f'{json.dumps(example, ensure_ascii=False)}\n')
         aio.run(main())
 
 
 def doc2query_postprocess(path, output):
-    task = QwQLongCoTDoc2QueryComputeScore(split="valid")
+    task=QwQLongCoTDoc2QueryComputeScore(split="valid")
 
     with open(output, "wt") as g:
         for filename in tqdm(os.listdir(path)):
-            filename = os.path.join(path, filename)
+            filename=os.path.join(path, filename)
             with open(filename, "rt") as f:
                 for line in f:
-                    example = json.loads(line)
-                    response = example["self_improvement"]["responses"][0]["response"]["text"]
-                    result = doc2query_parse_solution_fn(
+                    example=json.loads(line)
+                    response=example["self_improvement"]["responses"][0]["response"]["text"]
+                    result=doc2query_parse_solution_fn(
                         response, remove_option_letter=False)
                     if result is None:
                         continue
-                    question, options, answer = result
+                    question, options, answer=result
                     try:
-                        ans_index = task.MULTICHOICE_LETTER.index(answer)
+                        ans_index=task.MULTICHOICE_LETTER.index(answer)
                     except Exception as err:
                         continue
                     if len(options) < 40:
                         continue
                     if ans_index > len(options) - 1 or not options[ans_index].startswith(f'{answer})'):
                         continue
-                    result = doc2query_parse_solution_fn(
+                    result=doc2query_parse_solution_fn(
                         response, remove_option_letter=True)
-                    question, options, answer = result
-                    example["self_improvement"]["synthetic_qa_prompt"] = example["self_improvement"]["chat_completion"]
+                    question, options, answer=result
+                    example["self_improvement"]["synthetic_qa_prompt"]=example["self_improvement"]["chat_completion"]
                     del example["self_improvement"]["chat_completion"]
-                    example["self_improvement"]["synthetic_qa_response"] = response
-                    example["self_improvement"]["question"] = question
-                    answer_content = options[ans_index]
+                    example["self_improvement"]["synthetic_qa_response"]=response
+                    example["self_improvement"]["question"]=question
+                    answer_content=options[ans_index]
 
                     random.shuffle(options)
 
                     try:
-                        example["self_improvement"]["options"] = options
-                        example["self_improvement"]["answer"] = answer_content
-                        example["self_improvement"]["answer_letter"] = task.MULTICHOICE_LETTER[options.index(
+                        example["self_improvement"]["options"]=options
+                        example["self_improvement"]["answer"]=answer_content
+                        example["self_improvement"]["answer_letter"]=task.MULTICHOICE_LETTER[options.index(
                             answer_content)]
-                        example["self_improvement"]["prompt"] = 'Answer the following multiple choice question. There is only one correct answer. The last line of your response should be in the format "Answer: $LETTER" (without quotes), where LETTER is one of the option letters.\n\n' + task.format_question(
+                        example["self_improvement"]["prompt"]='Answer the following multiple choice question. There is only one correct answer. The last line of your response should be in the format "Answer: $LETTER" (without quotes), where LETTER is one of the option letters.\n\n' + task.format_question(
                             question=question, options=options, answer=None
                         )
 
