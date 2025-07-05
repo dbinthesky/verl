bae132f6cac14d0a360aaa361f0baa4e01d30248
dbinthesky <johntongjian@163.com>
2025-07-06 01:11:34 +0800

    update

--- rewards/fabricate_qa.py
+++ rewards/fabricate_qa.py
@@ -2304,7 +2304,7 @@ class Doc2QueryV2ComputeScoreWithQwQ32bRespondent(Doc2QueryV2ComputeScore):
             "request_kwargs": {
                 "temperature": 0.65,
                 "timeout": 600,
-                "max_tokens": 32768,
+                "max_tokens": 16384,
             },
         })
 
@@ -2330,7 +2330,7 @@ DOC2QUERY_QWQ32B_RESPONDENT_PARAMS = {
     "difficulty_run_args": {
         "w/o_content": {
             "model": Doc2QueryV2ComputeScoreWithQwQ32bRespondent.get_weak_agent(),
-            "repeat": 8,
+            "repeat": 10,
             "fn": Doc2QueryV2ComputeScoreWithQwQ32bRespondent.respond_wo_context,
             "desc": 'w/o ctx'
         },
@@ -2345,9 +2345,9 @@ DOC2QUERY_QWQ32B_RESPONDENT_PARAMS = {
         "advantage": 'w_content',
         "weakness": 'w/o_content',
         "advantage_oversimplified_threshold": 8/8,
-        "weakness_oversimplified_threshold": 7/8,
+        "weakness_oversimplified_threshold": 8/10,
         "advantage_overcomplex_threshold": 1/8,
-        "weakness_overcomplex_threshold": 1/8,
+        "weakness_overcomplex_threshold": 1/10,
         "advantage_threshold": 2/8,
         "advantage_weight": 0.0,
         "weakness_weight": 2.0,
