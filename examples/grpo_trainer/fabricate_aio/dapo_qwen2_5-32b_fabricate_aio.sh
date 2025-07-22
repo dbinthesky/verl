079e755aed96a426db1b0fb4c59cbd96b3fd7b4b
dbinthesky <johntongjian@163.com>
2025-07-22 22:19:02 +0800

    update

--- examples/grpo_trainer/fabricate_aio/dapo_qwen2_5-32b_fabricate_aio.sh
+++ examples/grpo_trainer/fabricate_aio/dapo_qwen2_5-32b_fabricate_aio.sh
@@ -52,7 +52,8 @@ setup_path() {
 
     CUSTOM_CODE_DIR="/cpfs01/shared/llm_ddd/tongjian/verl"
     VERL_DIR="/cpfs01/shared/llm_ddd/tongjian/verl"
-    BASE_MODEL_PATH="/cpfs01/shared/llm_ddd/tongjian/ckpts/DeepSeek-R1-Distill-Qwen-32B-fabricate_qa_v17"
+    # BASE_MODEL_PATH="/cpfs01/shared/llm_ddd/tongjian/ckpts/DeepSeek-R1-Distill-Qwen-32B-fabricate_qa_v17"
+    BASE_MODEL_PATH="/cpfs01/shared/llm_ddd/tongjian/ckpts/datareview_rl_test/verl/grpo/archived/fabricate_aio_0720_distillv17_roll16_bsz64_dapo_wo_kl_coef_wo_entropy_t8_step_45"
     TRAIN_DATA="/cpfs01/shared/llm_ddd/tongjian/rl/fabricate_aio/fabricate_aio_train_0718.parquet"
     VAL_DATA="/cpfs01/shared/llm_ddd/tongjian/rl/fabricate_aio/fabricate_aio_test_0718.parquet"
 
@@ -130,7 +131,7 @@ run_training() {
         actor_rollout_ref.rollout.name="vllm" \
         actor_rollout_ref.rollout.max_num_batched_tokens=300000 \
         actor_rollout_ref.rollout.gpu_memory_utilization=0.75 \
-        actor_rollout_ref.rollout.temperature=0.8 \
+        actor_rollout_ref.rollout.temperature=0.7 \
         actor_rollout_ref.rollout.n=16 \
         actor_rollout_ref.rollout.top_p=0.95 \
         actor_rollout_ref.ref.ulysses_sequence_parallel_size=2 \
